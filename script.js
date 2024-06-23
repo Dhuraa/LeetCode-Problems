ca421ce188f57a24b992147915b8a2ff7b0fd052
dhuraa <dhuramistry@gmail.com>
2024-06-23 22:29:13 +0530

    23 June 2024

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 var countKDifference = function(nums, k) {
-    
+    let count =0;
+    let left =0;
+    let right=nums.length-1;
+    while(left<right){
+        console.log(left,right);
+        if(Math.abs(nums[left] - nums[right]) == k){
+            console.log("in",nums[left], nums[right])
+            count++;
+            right--;
+        }
+        else{
+            console.log("else",nums[left], nums[right])
+            left++;
+        }
+    }
+    console.log(count);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+let nums = [1,2,2,1];
+let k = 1;
+
+countKDifference(nums,k);
